3cd0bdb605da82dc0593870ced35df28b5046c3d
kesavan <kesavtc@gmail.com>
2020-06-10 15:42:29 +0530

    resolution accepted

--- hungry.php
+++ hungry.php
@@ -5,10 +5,7 @@ echo "is it, are you Hungry?";
 
 echo "yes offcourse you are hungry, and you want to eat alot!";
 
-<<<<<<< HEAD
 echo "drink water";
-=======
 echo "another set of code change";
->>>>>>> thirsty
 
 ?>
